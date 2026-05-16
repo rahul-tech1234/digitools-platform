137a48ff919c58cf729df3eff48efc52133bb3d5
rahul-tech1234 <rahul.laravel.dev@gmail.com>
2026-05-17 01:01:23 +0600

    compleate design Banner section with figma

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import NavBar from "./components/NavBar/NavBar";
-
+import Banner from "./components/Banner/Banner";
 function App() {
  
   return (
     <>
     <NavBar></NavBar>
+    <Banner></Banner>
     
     
     
