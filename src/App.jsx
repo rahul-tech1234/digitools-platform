4401f7e9ce4fc2af2da7be0a1f06766df9b58bf9
rahul-tech1234 <rahul.laravel.dev@gmail.com>
2026-05-17 01:03:25 +0600

    design state component according to figma

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import NavBar from "./components/NavBar/NavBar";
 import Banner from "./components/Banner/Banner";
+import State from "./components/State/State";
 function App() {
  
   return (
     <>
     <NavBar></NavBar>
     <Banner></Banner>
+    <State></State>
     
     
     
---
+++ src/components/State/State.jsx
@@ -0,0 +1,25 @@
+const State = () => {
+    return (
+        <div className="stats shadow flex justify-center bg-blue-500 w-10/12 mx-auto h-28">
+  <div className="stat flex justify-around">
+    <div className="">
+        <h1 className='text-[white] font-extrabold stat-value text-wxl'>25.6K+</h1>
+        <p className='text-[white] font-medium'>Active Users</p>
+        </div>
+        <div className="">
+        <h1 className='text-[white] font-extrabold stat-value text-wxl'>25.6K+</h1>
+        <p className='text-[white] font-medium'>Premium Tools</p>
+        </div>
+        <div className="">
+        <h1 className='text-[white] font-extrabold stat-value text-wxl'>25.6K+</h1>
+        <p className='text-[white] font-medium'>Rating</p>
+        </div>
+  </div>
+
+  
+  
+</div>
+    );
+};
+
+export default State;
